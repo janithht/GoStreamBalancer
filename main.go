96d7deefe8f26ab9810bc17978cc84a5ef7c8a14
janithht <hathnagodahwemjn.20@uom.lk>
2024-03-27 11:05:10 +0530

    Cofig passed

--- main.go
+++ main.go
@@ -2,92 +2,47 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-	"net/http/httputil"
-	"net/url"
+	"log"
+	"os"
+	"time"
+
+	"gopkg.in/yaml.v2"
 )
 
-type Server interface {
-	Address() string
-	IsAlive() bool
-	Serve(rw http.ResponseWriter, req *http.Request)
+type Config struct {
+	Upstreams []Upstream `yaml:"upstreams"`
 }
 
-type simpleServer struct {
-	addr  string
-	proxy *httputil.ReverseProxy
+type Upstream struct {
+	Name        string   `yaml:"name"`
+	Servers     []string `yaml:"servers"`
+	HealthCheck `yaml:"healthCheck"`
+	RateLimit   `yaml:"rateLimit"`
 }
 
-func newSimpleServer(addr string) *simpleServer {
-	serverUrl, err := url.Parse(addr)
-	handleErr(err)
-
-	return &simpleServer{
-		addr:  addr,
-		proxy: httputil.NewSingleHostReverseProxy(serverUrl),
-	}
+type HealthCheck struct {
+	Url      string        `yaml:"url"`
+	Interval time.Duration `yaml:"interval"`
+	Timeout  time.Duration `yaml:"timeout"`
 }
 
-type loadBalancer struct {
-	port            string
-	roundRobincount int
-	servers         []Server
-}
-
-func newLoadBalancer(port string, servers []Server) *loadBalancer { //returns a pointer to the loadbalancer struct
-	return &loadBalancer{
-		port:            port,
-		roundRobincount: 0,
-		servers:         servers,
-	}
-}
-
-func handleErr(err error) {
-	if err != nil {
-		fmt.Printf("error: %v\n", err)
-	}
-}
-
-func (s *simpleServer) IsAlive() bool { return true }
-
-func (s *simpleServer) Serve(rw http.ResponseWriter, req *http.Request) {
-	s.proxy.ServeHTTP(rw, req)
-}
-
-func (lb *loadBalancer) getNextServer() Server {
-	server := lb.servers[lb.roundRobincount%len(lb.servers)]
-	for !server.IsAlive() {
-		lb.roundRobincount++
-		server = lb.servers[lb.roundRobincount%len(lb.servers)]
-	}
-
-	lb.roundRobincount++
-	return server
-}
-
-func (lb *loadBalancer) serveProxy(rw http.ResponseWriter, req *http.Request) {
-	targetServer := lb.getNextServer()
-	fmt.Printf("forwarding request to %s\n", targetServer.Address())
-	targetServer.Serve(rw, req)
+type RateLimit struct {
+	Limit    int           `yaml:"limit"`
+	Interval time.Duration `yaml:"interval"`
 }
 
 func main() {
-	servers := []Server{
-		newSimpleServer("https://www.facebook.com/"),
-		newSimpleServer("http://www.youtube.com/"),
-		newSimpleServer("https://www.amazon.com/"),
+	var config Config
+
+	data, err := os.ReadFile("config.yaml") // Read the file
+	if err != nil {
+		log.Fatalf("error: %v", err)
 	}
 
-	lb := newLoadBalancer("8080", servers)
-	handleRedirect := func(rw http.ResponseWriter, req *http.Request) {
-		lb.serveProxy(rw, req)
+	err = yaml.Unmarshal(data, &config) // Unmarshal the data to strcuts defined above
+	if err != nil {
+		log.Fatalf("error parsing config file: %v", err)
 	}
-	http.HandleFunc("/", handleRedirect)
 
-	fmt.Printf("Load Balancer listening on 'localhost:%s '\n", lb.port)
-	http.ListenAndServe(":"+lb.port, nil)
-}
-
-func (s *simpleServer) Address() string {
-	return s.addr
+	fmt.Println("Config parsed successfully:", config)
 }
