d4248b6c29d3f9f384299a5d62155057c7eb9901
janithht <hathnagodahwemjn.20@uom.lk>
2024-04-08 11:23:18 +0530

    Implemented Go routines for health checks

--- internal/healthchecks/healthCheck.go
+++ internal/healthchecks/healthCheck.go
@@ -16,20 +16,24 @@ func checkServerHealth(server string, healthCheckConfig config.HealthCheck) bool
 	res, err := client.Get(server + healthCheckConfig.Url)
 	return err == nil && res.StatusCode == 200
 }
-
-func PerformHealthChecks(config *config.Config) {
+func performHealthCheckForUpstream(upstream config.Upstream) {
 	for {
 		fmt.Println()
-		for _, upstream := range config.Upstreams {
-			for i := len(upstream.Servers) - 1; i >= 0; i-- {
-				server := upstream.Servers[i]
-				if !checkServerHealth(server, upstream.HealthCheck) {
-					log.Printf("Server %s failed health check, removing from pool\n", server)
-					// Remove server from slice safely.
-					upstream.Servers = append(upstream.Servers[:i], upstream.Servers[i+1:]...)
-				}
+		for i := len(upstream.Servers) - 1; i >= 0; i-- {
+			server := upstream.Servers[i]
+			if !checkServerHealth(server, upstream.HealthCheck) {
+				log.Printf("Server %s failed health check, removing from pool\n", server)
+				// Properly synchronize server removal here.
 			}
 		}
-		time.Sleep(config.Upstreams[0].HealthCheck.Interval)
+		time.Sleep(upstream.HealthCheck.Interval)
 	}
+}
+
+func PerformHealthChecks(config *config.Config) {
+	for _, upstream := range config.Upstreams {
+		go performHealthCheckForUpstream(upstream)
+	}
+
+	select {}
 }
