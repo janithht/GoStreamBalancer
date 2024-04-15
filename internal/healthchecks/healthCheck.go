0b098da0261363bf1d76d37c03d681b2d6803e29
janithht <hathnagodahwemjn.20@uom.lk>
2024-04-15 10:47:17 +0530

    Implemented independent healthCheck intervals for each upstream

--- internal/healthchecks/healthCheck.go
+++ internal/healthchecks/healthCheck.go
@@ -15,7 +15,11 @@ type HealthCheckTask struct {
 	HealthCheckConfig config.HealthCheck
 }
 
-var mutex = &sync.Mutex{}
+var (
+	mutex     = &sync.Mutex{}
+	taskQueue []HealthCheckTask
+	taskChan  chan HealthCheckTask
+)
 
 func checkServerHealth(server string, healthCheckConfig config.HealthCheck) bool {
 	client := http.Client{
@@ -46,40 +50,47 @@ func worker(ctx context.Context, id int, tasks <-chan HealthCheckTask) {
 
 func PerformHealthChecks(ctx context.Context, cfg *config.Config) {
 	const numWorkers = 10
-	taskQueue := make([]HealthCheckTask, 0)
-	taskChan := make(chan HealthCheckTask, 100)
+	taskQueue = make([]HealthCheckTask, 0)
+	taskChan = make(chan HealthCheckTask, 100)
 
 	for i := 0; i < numWorkers; i++ {
 		go worker(ctx, i, taskChan)
 	}
 
-	go func() {
-		for {
-			mutex.Lock()
-			if len(taskQueue) > 0 {
-				task := taskQueue[0]
-				taskQueue = taskQueue[1:]
-				mutex.Unlock()
-				taskChan <- task
-			} else {
-				mutex.Unlock()
-				time.Sleep(1 * time.Second)
-			}
-		}
-	}()
+	go manageQueue()
 
+	lastScheduled := make(map[string]time.Time)
 	for {
 		for _, upstream := range cfg.Upstreams {
-			for _, server := range upstream.Servers {
-				task := HealthCheckTask{
-					Server:            &server,
-					HealthCheckConfig: upstream.HealthCheck,
+			now := time.Now()
+			if lastTime, ok := lastScheduled[upstream.Name]; !ok || now.Sub(lastTime) >= upstream.HealthCheck.Interval {
+				for _, server := range upstream.Servers {
+					task := HealthCheckTask{
+						Server:            &server,
+						HealthCheckConfig: upstream.HealthCheck,
+					}
+					mutex.Lock()
+					taskQueue = append(taskQueue, task)
+					mutex.Unlock()
 				}
-				mutex.Lock()
-				taskQueue = append(taskQueue, task)
-				mutex.Unlock()
+				lastScheduled[upstream.Name] = now
 			}
-			time.Sleep(upstream.HealthCheck.Interval)
+		}
+		time.Sleep(time.Second) // Check every second if it's time to schedule next checks
+	}
+}
+
+func manageQueue() {
+	for {
+		mutex.Lock()
+		if len(taskQueue) > 0 {
+			task := taskQueue[0]
+			taskQueue = taskQueue[1:]
+			mutex.Unlock()
+			taskChan <- task
+		} else {
+			mutex.Unlock()
+			time.Sleep(1 * time.Second)
 		}
 	}
 }
